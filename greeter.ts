17bfb63a53dcae9bd3a1d8567bb7b31cbc5dbdef
Josh <joshbrogdon5@gmail.com>
2018-10-05 12:36:41 -0600

    adding a class

--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string){
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+}
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -7,6 +14,6 @@ function greeter(person: Person){
     return 'Hello, ' + person.firstName + ' ' + person.lastName;
 }
 
-let user = {firstName: 'Josh', lastName: 'Baggins'};
+let user = new Student('John', 'S.', 'Smith');
 
 document.body.innerHTML = greeter(user);
